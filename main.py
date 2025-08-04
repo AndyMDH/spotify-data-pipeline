5c2f556b02f6d7d9df267a8bc569953e24187396
Andy Ho <andy.hohuu.96@gmail.com>
2025-08-04 23:18:23 +0200

    add env example, polars and streamlit to pyproject

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from spotify-data-pipeline!")
+import spotipy
+import spotify.oauth2 as SpotifyOAuth
+import streamlit as st
+import polars as pl
+import os
 
+CLIENT_ID = os.getenv.get("CLIENT_ID")
+CLIENT_SECRET = os.getenv.get("CLIENT_SECRET")
+REDIRECT_URI = "http://localhost:8000"
 
-if __name__ == "__main__":
-    main()
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+                     client_id = CLIENT_ID, 
+                     client_secret = CLIENT_SECRET, 
+                     redirect_uri = REDIRECT_URI,
+                     scope = "user-top-read")
+                     )
+
+st.set_page_config(page_title="Spotify Data Pipeline", page_icon=":musical_note:")
